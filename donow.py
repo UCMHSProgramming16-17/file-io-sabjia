95d5003fd4c6613a5a01bd550abff871e648a153
Sabina Jia <sjia@ucvts.org>
2016-12-14 15:01:31 +0000

    do now

---
+++ donow.py
@@ -0,0 +1,12 @@
+# open file
+file = open('oikawa.txt', 'w')
+
+# add items
+file.write("i don't want them to go to different universities\n")
+file.write("honestly, i don't even think oikawa will be able to stand being more than two feet from iwaizumi\n")
+file.write("and iwaizumi is too protective and a worrywart sooooo\n")
+file.write("i want them both to go into the japan team together\n")
+file.write("but i don't think iwaizumi will want to join that team, i think he wants to be a doctor... i can imagine him as a doctor\n")
+
+# close file
+file.close()
